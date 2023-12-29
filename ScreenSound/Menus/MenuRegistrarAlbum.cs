8c752f687156bd66fc2f4b66d1e9205d70a0248b
RenanCruz <renancruz199@gmail.com>
2023-12-29 17:14:40 -0300

    Finalizando Curso

--- ScreenSound/Menus/MenuRegistrarAlbum.cs
+++ ScreenSound/Menus/MenuRegistrarAlbum.cs
@@ -10,10 +10,10 @@ internal class MenuRegistrarAlbum :Menu
         ExibirTituloDaOpcao("Registro de álbuns");
         Console.Write("Digite a banda cujo álbum deseja registrar: ");
         string nomeDaBanda = Console.ReadLine()!;
-        Console.Write("Agora digite o título do álbum: ");
-        string tituloAlbum = Console.ReadLine()!;
         if (bandasRegistradas.ContainsKey(nomeDaBanda))
         {
+            Console.Write("Agora digite o título do álbum: ");
+            string tituloAlbum = Console.ReadLine()!;
             Banda banda = bandasRegistradas[nomeDaBanda];
             banda.AdicionarAlbum(new Album(tituloAlbum));
             Console.WriteLine($"O álbum {tituloAlbum} de {nomeDaBanda} foi registrado com sucesso!");
